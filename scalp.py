919c9f5d6d2c08d4a274407a75e2a55c3d93bcae
beyman6 <78891978+beyman6@users.noreply.github.com>
2021-02-12 02:34:07 -0500

    Added email functionality

--- scalp.py
+++ scalp.py
@@ -1,13 +1,24 @@
 from selenium import webdriver
 import time
 from datetime import datetime
+import smtplib
+#Email Setup
+conn = smtplib.SMTP('smtp.gmail.com',587)
+conn.ehlo()
+conn.starttls()
+conn.login('gpustockcheck12@gmail.com','gPuP@sSw0rD')
+#Email Setup End
 browser = webdriver.Chrome()
 instock = False
 curtime = datetime.now()
 dt_string = curtime.strftime("%d/%m/%Y %H:%M:%S")
-
+usremail = ""
 page = input("Please enter the link for the item: ")
 def menu():
+    global usremail
+    emailch = input("Do you want to enable email notifs? (Y/N)")
+    if emailch == "Y":
+        usremail = input("Please enter your email in the form 'example@example.com': ")
     cont = True
     while cont:
         global page
@@ -47,15 +58,23 @@ def add_cart(buy_page):
         print("Out of stock")  
 def time_check(chk_page):
     global browser
+    global usremail
     check_stock(chk_page)
     while not instock:
         check_stock(chk_page)
         browser.refresh()
         time.sleep(60)
     if instock == True:
+        send_email()
+        
         add_cart(chk_page)
         time.sleep(5)
         elem = browser.find_element_by_xpath('//button[text()="Checkout"]')
         elem.click()
         time.sleep(10)
+
+def send_email():
+    global usremail
+    conn.sendmail('gpustockchecker12@gmail.com', usremail, 'Subject: INSTOCK NOTIFICATION \n\n Your item that you were following is in stock. Please go to your machine and enter payment info to secure the item. \n\n This is an automated email, please do not reply.')
+    print(f"An email has been sent to {usremail}")
 menu()
